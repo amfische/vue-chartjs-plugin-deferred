4c1327ea0371ad8c183e8426f37255617c366ee8
amfische <amfischer21@gmail.com>
2018-11-22 10:37:19 -0700

    needed to import the plugin and register it to use it

--- src/components/bar-chart.vue
+++ src/components/bar-chart.vue
@@ -1,10 +1,12 @@
 <script>
 	import { Bar } from 'vue-chartjs'
+	import deferred from 'chartjs-plugin-deferred'
 
 	export default {
 		extends: Bar,
 		props: ['chartData', 'options'],
 		mounted() {
+			this.addPlugin(deferred)
 			this.renderChart(this.chartData, this.options)
 		}
 	}
